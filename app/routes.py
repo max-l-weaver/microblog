5e3e494984d31a3dbb9ed84d826c4b48e2a88b8c
Max Weaver <max.weaver@infectiousmedia.com>
2019-12-30 23:12:02 +0000

    Chapter 2: forgot to add updated routes

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {"username": "Miguel"}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author' : {'username': 'John'},
+            'body': 'A beautiful day in Portland, OR'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
