de227637360b520526c984716a37b2a89f4126d8
Max Weaver <max.weaver@infectiousmedia.com>
2019-12-31 00:01:37 +0000

    Chapter 3: Adding basic form data using wtforms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,7 +8,7 @@ def index():
     user = {"username": "Miguel"}
     posts = [
         {
-            'author' : {'username': 'John'},
+            'author': {'username': 'John'},
             'body': 'A beautiful day in Portland, OR'
         },
         {
@@ -16,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
